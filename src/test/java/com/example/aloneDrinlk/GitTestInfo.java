7e38c1e4be64ca17fcf4739d33637ae38b1136f0
DESKTOP-PSSMJ3K\PC <jh950925@naver.com>
2023-06-19 17:57:38 +0900

    깃허브 연동 잔디 api 데이터 불러오기

--- src/test/java/com/example/aloneDrinlk/GitTestInfo.java
+++ src/test/java/com/example/aloneDrinlk/GitTestInfo.java
@@ -6,12 +6,13 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class GitTestInfo {
@@ -20,12 +21,14 @@ public class GitTestInfo {
 
         try {
             // 잔디 정보 API 엔드포인트 URL 생성
-            String apiUrl = "https://api.github.com/users/" + username + "/contributions";
+            String apiUrl = "https://api.github.com/users/" + username;
+
+            log.info("apiUrl : " + apiUrl);
 
             // API 호출 및 데이터 가져오기
             String jsonResponse = sendGetRequest(apiUrl);
 
-            log.info(jsonResponse);
+            log.info("jsonResponse"+ jsonResponse);
 
             // JSON 파싱
             JsonArray jsonArray = new Gson().fromJson(jsonResponse, JsonArray.class);
@@ -50,15 +53,29 @@ public class GitTestInfo {
 
     // GET 요청을 보내고 응답 데이터를 문자열로 반환하는 메소드
     private static String sendGetRequest(String url) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            return IOUtils.toString(connection.getInputStream(), StandardCharsets.UTF_8);
+//        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+//        connection.setRequestMethod("GET");
+//        connection.connect();
+//        int responseCode = connection.getResponseCode();
+//        if (responseCode == HttpURLConnection.HTTP_OK) {
+//            return IOUtils.toString(connection.getInputStream(), StandardCharsets.UTF_8);
+//        } else {
+//            throw new IOException("GET request failed with response code: " + responseCode);
+//        }
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> entity = new HttpEntity<>(null, null);
+        log.info("1");
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        log.info("2");
+        log.info("response : " + response);
+        log.info("=============");
+        String body = response.getBody();
+        log.info("body : " + body);
+        log.info("=============");
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return body;
         } else {
-            throw new IOException("GET request failed with response code: " + responseCode);
+            throw new IOException("GET request failed with response code: " + body);
         }
     }
 }
