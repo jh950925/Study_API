2a2079be615cd8721f66bec713f4fad2a6597baf
DESKTOP-PSSMJ3K\PC <jh950925@naver.com>
2023-06-19 16:51:33 +0900

    스프링 시큐리티 설정

--- src/main/java/com/example/aloneDrinlk/security/SecurityConfig.java
+++ src/main/java/com/example/aloneDrinlk/security/SecurityConfig.java
@@ -1,41 +1,42 @@
-//package com.example.aloneDrinlk.security;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//@Slf4j
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final JwtTokenProvider jwtTokenProvider;
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic().disable()
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+package com.example.aloneDrinlk.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/chk").permitAll()    // LoadBalancer Chk
+//                .antMatchers("/manage").hasAuthority("ADMIN") //ADMIN 설정
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+//                .loginPage("/view/login") // 커스텀 페이지로 로그인 페이지 변경
+//                .loginProcessingUrl("/loginProc") // form 태그에서 action = "loginProc" 지정하면 자동으로 id pass 보내준다.
+//                .usernameParameter("id") // 기본 input type
+//                .passwordParameter("pw") // 기본 input type
+                .defaultSuccessUrl("/main", true)
+                .permitAll()
 //                .and()
-//                .authorizeRequests()
-//                .mvcMatchers("/api/member/login").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//}
+//                .logout() // 로그아웃 uri 설정
+        ;
+    }
+
+    /**
+     * 시큐리티 예외처리
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
+    }
+}
