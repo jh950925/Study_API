d6bd8c9bc275b1e514ed92e534326bf8ac4fb9d5
DESKTOP-PSSMJ3K\PC <jh950925@naver.com>
2023-06-20 15:42:34 +0900

    깃 푸쉬 데이터 받아오기

--- src/test/java/com/example/aloneDrinlk/GitTestInfo.java
+++ src/test/java/com/example/aloneDrinlk/GitTestInfo.java
@@ -1,10 +1,8 @@
 package com.example.aloneDrinlk;
 
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -13,9 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor
 public class GitTestInfo {
+
+
+    private final ObjectMapper objectMapper;
+
+
     public static void main(String[] args) {
         String username = "jh950925"; // 대상 사용자의 GitHub 사용자 이름
 
@@ -26,56 +33,65 @@ public class GitTestInfo {
             log.info("apiUrl : " + apiUrl);
 
             // API 호출 및 데이터 가져오기
-            String jsonResponse = sendGetRequest(apiUrl);
+            Map<String, Object> gitData = sendGetRequest(apiUrl);
+            String eventUrl = gitData.get("events_url").toString();
+            log.info("eventUrl : " + eventUrl);
+            log.info("jsonResponse"+ gitData);
 
-            log.info("jsonResponse"+ jsonResponse);
+            List<Map<String,Object>> gitEventDaa = sendGetRequestEvent(eventUrl);
 
-            // JSON 파싱
-            JsonArray jsonArray = new Gson().fromJson(jsonResponse, JsonArray.class);
+            log.info("gitEventDaa : " + gitEventDaa);
 
-            // 커밋 수와 날짜 추출 및 가공
-            for (JsonElement jsonElement : jsonArray) {
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                String date = jsonObject.get("date").getAsString();
-                int commitCount = jsonObject.get("count").getAsInt();
+            ArrayList<String> arrayList = new ArrayList<>();
 
-                // TODO: 원하는 가공 작업 수행
-
-                // 예시: 출력
-                System.out.println("Date: " + date);
-                System.out.println("Commit Count: " + commitCount);
-                System.out.println("------------------------------");
+            for (Map map : gitEventDaa) {
+                Map<String, Object> payloadMap = (Map<String, Object>) map.get("payload");
+                List<Map<String, Object>> commitsList = (List<Map<String, Object>>) payloadMap.get("commits");
+                if (commitsList != null) {
+                    for (Map<String, Object> commitMap : commitsList) {
+                        String message = (String) commitMap.get("message");
+                        log.info("Message: " + message);
+                        arrayList.add(message);
+                        // 메시지를 활용하여 원하는 작업을 수행합니다.
+                    }
+                }
             }
+
+            log.info("arrayList : " + arrayList);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     // GET 요청을 보내고 응답 데이터를 문자열로 반환하는 메소드
-    private static String sendGetRequest(String url) throws IOException {
-//        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-//        connection.setRequestMethod("GET");
-//        connection.connect();
-//        int responseCode = connection.getResponseCode();
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            return IOUtils.toString(connection.getInputStream(), StandardCharsets.UTF_8);
-//        } else {
-//            throw new IOException("GET request failed with response code: " + responseCode);
-//        }
+    // 유저 정보 기준해서 데이터 받아오기
+    private static Map<String,Object> sendGetRequest(String url) throws IOException {
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> entity = new HttpEntity<>(null, null);
-        log.info("1");
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        log.info("2");
-        log.info("response : " + response);
-        log.info("=============");
-        String body = response.getBody();
-        log.info("body : " + body);
-        log.info("=============");
+
+        ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.GET, entity, Map.class);
+        Map<String, Object> parseBody = response.getBody();
+
         if (response.getStatusCode() == HttpStatus.OK) {
-            return body;
+            return parseBody;
         } else {
-            throw new IOException("GET request failed with response code: " + body);
+            throw new IOException("GET request failed with response code: " + parseBody);
         }
+    }
+
+    // 깃허브 푸쉬 이벤트 가져오기
+    private static List<Map<String,Object>> sendGetRequestEvent(String url) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        log.info("url : " + url);
+        HttpEntity<String> httpEntity = new HttpEntity<>(null, null);
+
+        ResponseEntity<List> response = restTemplate.exchange(url.replaceAll("\\{\\/privacy\\}",""), HttpMethod.GET, httpEntity, List.class);
+        List<Map<String, Object>> parseBody = response.getBody();
+
+        log.info("=====\n" + parseBody);
+
+        return parseBody;
     }
 }
